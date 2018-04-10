7ca1f82f31727b37a9943776b5881c7a7d9fefb3
Your Actual Name, In Quotes <Your Actual Email, In Quotes>
2018-04-10 23:33:43 +0200

    oinfcon

---
+++ Goldman.rb
@@ -0,0 +1,5 @@
+Price = ([17,3,6,9,15,8,6,1,10])
+
+low = Price.min
+
+buy_day = Price.index(low)
---
+++ Multiples.rb
@@ -0,0 +1,2 @@
+answer = (0..999).select {|n| n%3==0 || n%5==0}
+puts answer.inject {|sum, n| sum + n}
